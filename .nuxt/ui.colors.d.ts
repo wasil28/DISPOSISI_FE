b139e90ec6a6e0bcd2a83edbdec4c8b56c115854
wasil <wasilmawardi0@gmail.com>
2025-04-22 10:08:11 +0700

    fix : config

--- .nuxt/ui.colors.d.ts
+++
@@ -1,1 +0,0 @@
-declare module '#ui-colors' { const defaultExport: ["red","orange","amber","yellow","lime","green","emerald","teal","cyan","sky","blue","indigo","violet","purple","fuchsia","pink","rose","ut","action","picton-blue","primary"]; export default defaultExport; }
